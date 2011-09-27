465cf12e22217ecc3feb4bff76b70c881ba9bdf0
Nolan Caudill <nolan@nolancaudill.com>
2011-09-27 16:46:36 -0700

    removing some unneeded files

--- site/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    require 'src/init.php';
-
-    $flickr = new Flickr($GLOBALS['cfg']['flickr_key'], $GLOBALS['cfg']['flickr_secret']);
-
-    $res = $flickr->call_method('flickr.push.getTopics');
-
