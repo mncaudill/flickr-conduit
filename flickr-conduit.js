4709bae7021b0b3dfd57197723db43d40f0dc9e3
Nolan Caudill <nolan@nolancaudill.com>
2012-09-12 14:55:34 -0700

    Updated to match Flickr's new atom output (thanks crawft)
    The code for the change came from crawft attached to
    https://github.com/mncaudill/flickr-conduit/issues/2

--- flickr-conduit.js
+++ flickr-conduit.js
@@ -76,31 +76,29 @@ var parseFlickrPost = function(content, callback) {
     var xml = new xml2js.Parser();
     var imgObjs = [];
     xml.on('end', function(data) {
-        try {
-            // We possibly get multiple entries per POST
-            var entries = Array.isArray(data.entry) ? data.entry : [data.entry];
+		try {
+			var entries = Array.isArray(data.feed.entry) ? data.feed.entry : [data.feed.entry]; 
+			var imgData = null;
+			var photoUrl= null;
+			for (var i in entries) {
+				imgData = entries[i]['media:content'][0]['$'];
 
-            var imgData = null;
-            var photoUrl= null;
-            for (var i in entries) {
-                    imgData = entries[i]['media:content']['@'];
+				// Dumb, but there's a bug in the xml2js that messes up on the <link> tab. (Or I'm missing something.)
+				var id = entries[i]['id'][0].split(':')[2].split('/')[2];
+				photoUrl = entries[i].author[0].uri[0].replace("http://www.flickr.com/people/", 'http://www.flickr.com/photos/');
+				photoUrl += id + '/';
 
-                    // Dumb, but there's a bug in the xml2js that messes up on the <link> tab. (Or I'm missing something.)
-                    var id = entries[i]['id'].split(':')[2].split('/')[2];
-                    photoUrl = entries[i].author.uri.replace("http://www.flickr.com/people/", 'http://www.flickr.com/photos/');
-                    photoUrl += id + '/';
-
-                    imgObjs.push({
-                        url: imgData.url,
-                        width: imgData.width,
-                        height: imgData.height,
-                        link: photoUrl,
-                        raw: entries[i],
-                    });
-            }
-        } catch (e) {
-            // Noop
-        }
+				imgObjs.push({
+					url: imgData.url,
+					width: imgData.width,
+					height: imgData.height,
+					link: photoUrl,
+					raw: entries[i],
+				});
+			}
+		} catch (e) {
+			// Noop
+		}
         callback(imgObjs);
     });
 
